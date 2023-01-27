0739045d118947c2e8d53c80d4cbfeda8706a401
Vítor Alves <alves.vitor@bcg.com>
2023-01-27 17:01:47 -0300

    integration test and controller test

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const res = require('express/lib/response');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const todoRoutes = require('./routes/todo.routes');
 
 app.get('/', (request, response) => {
   response.json({ message: 'hello world!!' });
 });
 
+app.use('/todos', todoRoutes);
+
 app.listen(3000, () => {
   console.log('Server is now running');
 });
+
+module.exports = app;
--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,5 +1,7 @@
 const TodoModel = require('../model/todo.model');
 
-exports.createTodo = () => {
-  TodoModel.create({});
+exports.createTodo = (req, res, next) => {
+  console.log('req:', req.body);
+  const todo = TodoModel.create(req.body);
+  res.status(201).json(todo);
 };
