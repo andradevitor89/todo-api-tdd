ca6ac9be5c3baf081a953e86fa734dc5b55b2f0d
Vítor Alves <alves.vitor@bcg.com>
2023-01-29 12:36:33 -0300

    decoupling integration tests

--- test/integration/todo.controller.int.test.js
+++ test/integration/todo.controller.int.test.js
@@ -2,7 +2,6 @@ const request = require('supertest');
 const app = require('../../app');
 const newTodo = require('../mock-data/new-todo.json');
 const endpointUrl = '/todos/';
-let firstTodo, createdTodo;
 
 describe('POST ' + endpointUrl, () => {
   it('should return success', async () => {
@@ -12,11 +11,9 @@ describe('POST ' + endpointUrl, () => {
     expect(response.statusCode).toBe(201);
     expect(response.body.title).toBe(newTodo.title);
     expect(response.body.done).toBe(newTodo.done);
-    createdTodo = response.body;
   });
   it('should return error 500 on malformed data', async () => {
-    const malformedPayload = newTodo;
-    malformedPayload.done = null;
+    const malformedPayload = { ...newTodo, done: null };
     const response = await request(app)
       .post(endpointUrl)
       .send(malformedPayload);
@@ -34,18 +31,18 @@ describe('GET ' + endpointUrl, () => {
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].title).toBeDefined();
     expect(response.body[0].done).toBeDefined();
-    firstTodo = response.body[0];
   });
 });
 
 describe('GET ' + endpointUrl + ':id', () => {
   it('should return success', async () => {
+    const newCreatedTodo = await createMockTodo();
     const response = await request(app).get(
-      endpointUrl + firstTodo._id
+      endpointUrl + newCreatedTodo._id
     );
     expect(response.statusCode).toBe(200);
-    expect(response.body.title).toBe(firstTodo.title);
-    expect(response.body.done).toBe(firstTodo.done);
+    expect(response.body.title).toBe(newCreatedTodo.title);
+    expect(response.body.done).toBe(newCreatedTodo.done);
   });
   it('should return 204 when id is not found', async () => {
     const response = await request(app).get(
@@ -57,12 +54,13 @@ describe('GET ' + endpointUrl + ':id', () => {
 
 describe('PUT ' + endpointUrl + ':id', () => {
   it('should return success', async () => {
+    const newCreatedTodo = await createMockTodo();
     const payload = {
       title: 'New name',
       done: true,
     };
     const response = await request(app)
-      .put(endpointUrl + firstTodo._id)
+      .put(endpointUrl + newCreatedTodo._id)
       .send(payload);
 
     expect(response.statusCode).toBe(200);
@@ -85,13 +83,15 @@ describe('PUT ' + endpointUrl + ':id', () => {
 
 describe('DELETE ' + endpointUrl + ':id', () => {
   it('should return 200 with deleted todo', async () => {
+    const newCreatedTodo = await createMockTodo();
+
     const response = await request(app).delete(
-      endpointUrl + createdTodo._id
+      endpointUrl + newCreatedTodo._id
     );
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.title).toBe(createdTodo.title);
-    expect(response.body.done).toBe(createdTodo.done);
+    expect(response.body.title).toBe(newCreatedTodo.title);
+    expect(response.body.done).toBe(newCreatedTodo.done);
   });
   it('should return 404 when todo is not found', async () => {
     const response = await request(app).delete(
@@ -101,3 +101,10 @@ describe('DELETE ' + endpointUrl + ':id', () => {
     expect(response.statusCode).toBe(404);
   });
 });
+
+async function createMockTodo() {
+  const response = await request(app)
+    .post(endpointUrl)
+    .send(newTodo);
+  return response.body;
+}
