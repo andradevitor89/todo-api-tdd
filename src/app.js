20598b3beef73c5cb0c075d43cd96b97d4fc4e94
Vítor Alves <alves.vitor@bcg.com>
2023-01-30 11:15:15 -0300

    moving source code to src/ directory trying to add cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@ const express = require('express');
 const mongodb = require('./mongodb/mongodb.connect');
 const todoRoutes = require('./routes/todo.routes');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDefinition = require('./swagger.json');
+const swaggerDefinition = require('../swagger.json');
+const cors = require('cors');
 const errorHandlingMiddleware = require('./middlewares/error-handling.middleware');
+
 mongodb.connect();
 
 const app = express();
+app.use(errorHandlingMiddleware);
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use('/todos', todoRoutes);
@@ -15,7 +24,6 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(swaggerDefinition)
 );
-app.use(errorHandlingMiddleware);
 
 app.get('/', (request, response) => {
   response.json({ message: 'API is ON!' });
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ const app = require('./app');
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server is now running on :${port}`);
+  console.log(
+    `[${new Date().toISOString()}]Server is now running on :${port}`
+  );
 });
